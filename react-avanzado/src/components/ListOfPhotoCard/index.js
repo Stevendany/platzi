ba927aa171fc9f7431c6a897fd3a9d1cc9e0165a
@Stevendany <stevendany@outlook.com>
2020-12-09 19:12:10 -0600

    [graphQl,Apollo]

--- react-avanzado/src/components/ListOfPhotoCard/index.js
+++ react-avanzado/src/components/ListOfPhotoCard/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-export const ListOfPhotoCards = () => {
+const withPhotos = graphql(gql`
+query getPhotos{
+  photos{
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
-      {[1, 2, 3, 6, 7, 8].map(photoCard =>
-        <li key={photoCard}>
-          <PhotoCard />
-        </li>)}
+      {photos.map(photo =>
+        <PhotoCard key={photo.id} {...photo} />
+      )}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
--- react-avanzado/src/components/PhotoCard/index.js
+++ react-avanzado/src/components/PhotoCard/index.js
@@ -1,26 +1,16 @@
 import React, { useEffect, useRef, useState, Fragment } from 'react'
 import { Article, ImgWrapper, Button, Img } from './style'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../hooks/useLocalStorage'
+import { useNearScreen } from '../hooks/useNearScreen'
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
-  useEffect(function () {
-    Promise.resolve(typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer'))
-      .then(() => {
-        const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0]
-          if (isIntersecting) {
-            console.log('yesh')
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(element.current)
-      })
-  }, [element])
+  const [show, element] = useNearScreen()
+  const key = `liked-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -31,8 +21,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
               <Img src={src} alt='img' />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='27' /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='27' /> {likes} likes!
           </Button>
         </>
       }
